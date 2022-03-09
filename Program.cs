9d92fad4c803dde94c5991de2e405580a9eab562
netonblpb <skullbog@gmail.com>
2022-03-09 11:54:03 +0300

    Task43 complete

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу преобразования десятичного числа в двоичное.
+
+Console.Clear();
+
+Console.Write("Введите число, которое нужно преобразовать: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите основание новой системы счисления: ");
+int osn = int.Parse(Console.ReadLine());
+string resultReverse = "";
+string result = "";
+
+string Transform(int n, int o, string res)
+{
+    while (n > 0)
+    {
+        res += n % o;
+        n /= o;
+    }
+    return res;
+}
+
+string Turn(string res, string change)
+{
+    for (int i = res.Length; i > 0; i--)
+    {
+        change += res[i];
+    }
+    return change;
+}
+
+Console.WriteLine(Transform(num, osn, resultReverse));
+Console.WriteLine(Turn(resultReverse, result));
